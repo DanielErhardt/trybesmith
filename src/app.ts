e28d8308c0da80cd488a56ad4097927415be4dab
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-14 00:21:19 -0300

    feat: Imported errorHandler middleware into app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler';
 import productsRouter from './routers/products';
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', productsRouter);
+
+app.use(errorHandler);
 
 export default app;
