e440f9ed0c0c3ae7d33a4198e3fd09d7ea213e0c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-13 23:59:50 -0300

    feat: Created a middlewares file and implemented an error middleware.

---
+++ src/middlewares/index.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import status from 'http-status';
+import HttpException from '../utilities/HttpException';
+
+const errorHandler = async (err: Error, req: Request, res: Response, _next: NextFunction) => {
+  const { httpStatus, message } = err as HttpException;
+  return httpStatus
+    ? res.status(httpStatus).json({ message })
+    : res.status(status.INTERNAL_SERVER_ERROR).json({ message: 'Run to the hills!' });
+};
+
+export default { errorHandler };
