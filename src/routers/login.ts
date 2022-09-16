36b55a3966f16aef3c6b1df643a55b566d6fa4c6
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-16 16:40:27 -0300

    fix: Changed /login/ route request type to post.

--- src/routers/login.ts
+++ src/routers/login.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import rescue from 'express-rescue';
 import usersController from '../controllers/users';
 import validateLogin from '../middlewares/validateLogin';
 
 const loginRouter = Router();
 
-loginRouter.get('/', validateLogin, usersController.login);
+loginRouter.post('/', validateLogin, rescue(usersController.login));
 
 export default loginRouter;
