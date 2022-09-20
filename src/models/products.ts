3cfa32f4073eb275134271f00334eb77db56af3d
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-09-19 23:27:10 -0300

    feat: Implemented new functions on products model. One for verifying if all products with ids in a given array exist, another for updating the orderId column.

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,6 +1,8 @@
 import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import httpStatus from 'http-status';
 import connection from './connection';
-import { Product } from '../types';
+import { Product, Order } from '../types';
+import HttpException from '../utilities/HttpException';
 
 const getAll = async (): Promise<RowDataPacket[]> => {
   const [rowDataPacket] = await connection
@@ -19,7 +21,32 @@ const create = async (product: Product): Promise<ResultSetHeader> => {
   return result;
 };
 
+const productsExist = async (productsIds: number[]): Promise<boolean> => {
+  const [[result]] = await connection.query<RowDataPacket[]>(
+    'SELECT COUNT(*) FROM Trybesmith.Products WHERE id IN (?);',
+    [productsIds],
+  );
+  return result['COUNT(*)'] === productsIds.length;
+};
+
+const updateOrderId = async (order: Order): Promise<void> => {
+  if (!order.id) {
+    throw new HttpException(httpStatus.UNPROCESSABLE_ENTITY, 'Order id must be defined.');
+  }
+
+  const queries = order.productsIds.map((productId) => connection.execute(
+    `UPDATE Trybesmith.Products
+    SET orderId = ?
+    WHERE id = ?;`,
+    [order.id, productId],
+  ));
+
+  await Promise.all(queries);
+};
+
 export default {
   getAll,
   create,
+  productsExist,
+  updateOrderId,
 };
